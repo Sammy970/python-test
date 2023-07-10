981998a2b5439f3031d7d5f13c58028bd1d184d9
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-10 11:23:33 +0530

    Rollback

--- api/hello.py
+++ api/hello.py
@@ -9,17 +9,8 @@ def get_og_tags():
     # Get the URL from the query parameter
     url = request.args.get('url')
 
-    # Define the proxy URL
-    proxy_url = 'http://20.204.212.45:3129'
-
-    # Set up the proxy
-    proxy = {
-        'http': proxy_url,
-        'https': proxy_url
-    }
-
     # Make a request to the URL
-    response = requests.get(url, proxies=proxy)
+    response = requests.get(url)
     response.raise_for_status()
 
     # Create a BeautifulSoup object
